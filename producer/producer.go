f3ef419d3cb832b71842a3bc0acb81647a921d9e
박기현 <kihyun1998@naver.com>
2024-07-26 09:31:06 +0900

    수집 데이터 수 개선, 시간값 개선, 무한루프로 개선

--- producer/producer.go
+++ producer/producer.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 
 	"github.com/IBM/sarama"
@@ -16,6 +17,8 @@ const (
 	kafkaTopic      = "btcusdt-1m-candles"
 	maxRetries      = 5
 	retryDelay      = 5 * time.Second
+	fetchInterval   = 1 * time.Minute
+	candleLimit     = 300
 )
 
 type CandleData struct {
@@ -39,7 +42,7 @@ func init() {
 }
 
 func fetchBTCCandleData() ([]CandleData, error) {
-	url := fmt.Sprintf("%s?symbol=BTCUSDT&interval=1m&limit=200", binanceKlineAPI)
+	url := fmt.Sprintf("%s?symbol=BTCUSDT&interval=1m&limit=%d", binanceKlineAPI, candleLimit)
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -73,19 +76,24 @@ func fetchBTCCandleData() ([]CandleData, error) {
 	return candles, nil
 }
 
-func produceToKafka(producer sarama.SyncProducer, candle CandleData) error {
-	jsonData, err := json.Marshal(candle)
-	if err != nil {
-		return err
-	}
+func produceToKafka(producer sarama.SyncProducer, candles []CandleData) error {
+	for _, candle := range candles {
+		jsonData, err := json.Marshal(candle)
+		if err != nil {
+			return err
+		}
 
-	msg := &sarama.ProducerMessage{
-		Topic: kafkaTopic,
-		Value: sarama.StringEncoder(jsonData),
-	}
+		msg := &sarama.ProducerMessage{
+			Topic: kafkaTopic,
+			Value: sarama.StringEncoder(jsonData),
+		}
 
-	_, _, err = producer.SendMessage(msg)
-	return err
+		_, _, err = producer.SendMessage(msg)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func connectProducer(brokers []string) (sarama.SyncProducer, error) {
@@ -103,6 +111,15 @@ func connectProducer(brokers []string) (sarama.SyncProducer, error) {
 	return nil, fmt.Errorf("failed to connect to Kafka after %d attempts", maxRetries)
 }
 
+func utcToLocal(utcTime time.Time) time.Time {
+	loc, err := time.LoadLocation("Asia/Seoul")
+	if err != nil {
+		fmt.Printf("Error loading location: %v\n", err)
+		return utcTime
+	}
+	return utcTime.In(loc)
+}
+
 func main() {
 	producer, err := connectProducer([]string{kafkaBroker})
 	if err != nil {
@@ -110,20 +127,40 @@ func main() {
 	}
 	defer producer.Close()
 
-	candles, err := fetchBTCCandleData()
-	if err != nil {
-		fmt.Printf("Error fetching candle data: %v\n", err)
-		return
-	}
+	ticker := time.NewTicker(fetchInterval)
+	defer ticker.Stop()
 
-	for _, candle := range candles {
-		err = produceToKafka(producer, candle)
-		if err != nil {
-			fmt.Printf("Error producing to Kafka: %v\n", err)
-		} else {
-			fmt.Printf("Successfully sent candle data to Kafka: OpenTime %v\n", candle.OpenTime)
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+
+	for {
+		select {
+		case <-ticker.C:
+			candles, err := fetchBTCCandleData()
+			if err != nil {
+				fmt.Printf("Error fetching candle data: %v\n", err)
+				continue
+			}
+
+			err = produceToKafka(producer, candles)
+			if err != nil {
+				fmt.Printf("Error producing to Kafka: %v\n", err)
+			} else {
+				fmt.Printf("Successfully sent %d candle data to Kafka\n", len(candles))
+				if len(candles) > 0 {
+					firstCandle := candles[0]
+					lastCandle := candles[len(candles)-1]
+					firstTime := utcToLocal(time.Unix(firstCandle.OpenTime/1000, 0))
+					lastTime := utcToLocal(time.Unix(lastCandle.CloseTime/1000, 0))
+					fmt.Printf("Data range (Local Time): %v to %v\n",
+						firstTime.Format("2006-01-02 15:04:05"),
+						lastTime.Format("2006-01-02 15:04:05"))
+				}
+			}
+
+		case <-signals:
+			fmt.Println("Interrupt received, shutting down...")
+			return
 		}
 	}
-
-	fmt.Println("Finished sending all candle data to Kafka")
 }
